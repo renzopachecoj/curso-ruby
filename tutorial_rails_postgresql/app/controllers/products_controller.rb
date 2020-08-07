d1fdb0456c8f979282b0539ab28bc0251a955941
Renzo Pacheco <renzopachecoj@gmail.com>
2020-08-06 19:27:49 -0700

    crud postgresql

--- tutorial_rails_postgresql/app/controllers/products_controller.rb
+++ tutorial_rails_postgresql/app/controllers/products_controller.rb
@@ -2,4 +2,35 @@ class ProductsController < ApplicationController
     def index
         @products = Product.all
     end
+    def new
+        @products = Product.new
+    end
+    def create
+        @products = Product.new(products_params)
+        if @products.save
+            redirect_to products_path
+        else
+            render :new
+        end
+    end
+    def edit
+        @product = Product.find(params[:id])
+    end
+    def update
+        @products = Product.find(params[:id])
+        if @products.update(products_params)
+            redirect_to products_path
+        else
+            render :edit
+        end
+    end
+    def delete
+        @products = Product.find(params[:id])
+        @products.destroy
+        redirect_to products_path
+    end
+    private
+        def products_params
+            params.require(:product).permit(:name, :description, :price)
+        end
 end
--- tutorial_rails_postgresql/config/routes.rb
+++ tutorial_rails_postgresql/config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   get '/products', to:'products#index'
+  get '/products/new', to: 'products#new', as: 'new_product'
+  post '/products', to: 'products#create'
+  get '/products/:id/edit', to: 'products#edit', as:'edit_product'
+  get '/products/:id', to:'products#show', as:'product'
+  patch 'products/:id', to: 'products#update'
+  put '/product/:id', to: 'products#update'
+  delete '/products/:id', to: 'products#delete'
   #resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
